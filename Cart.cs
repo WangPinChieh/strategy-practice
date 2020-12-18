f8f8d92fcf4b7cc2d449e1f3a0e80a09e55dc3ff
WangPinChieh <pp8101313@hotmail.com>
2020-12-18 22:29:18 +0800

    refactoring: use switch statement instead of if statement

--- Cart.cs
+++ Cart.cs
@@ -4,27 +4,30 @@ namespace strategy_practice
 {
     public class Cart
     {
-        public double shippingFee(string shipper, double length, double width, double height, double weight) {
-            if (shipper == "black cat") {
-                if (weight > 20) {
-                    return 500;
-                } else {
-                    return 100 + weight * 10;
+        public double shippingFee(string shipper, double length, double width, double height, double weight)
+        {
+            switch (shipper)
+            {
+                case "black cat":
+                     return weight > 20 ? 500 : 100 + weight * 10;
+                case "hsinchu":
+                {
+                    double size = length * width * height;
+                    if (length > 100 || width > 100 || height > 100) {
+                        return size * 0.00002 * 1100 + 500;
+                    } else {
+                        return size * 0.00002 * 1200;
+                    }
                 }
-            } else if (shipper == "hsinchu") {
-                double size = length * width * height;
-                if (length > 100 || width > 100 || height > 100) {
-                    return size * 0.00002 * 1100 + 500;
-                } else {
-                    return size * 0.00002 * 1200;
+                case "post office":
+                {
+                    double feeByWeight = 80 + weight * 10;
+                    double size = length * width * height;
+                    double feeBySize = size * 0.00002 * 1100;
+                    return feeByWeight < feeBySize ? feeByWeight : feeBySize;
                 }
-            } else if (shipper == "post office") {
-                double feeByWeight = 80 + weight * 10;
-                double size = length * width * height;
-                double feeBySize = size * 0.00002 * 1100;
-                return feeByWeight < feeBySize ? feeByWeight : feeBySize;
-            } else {
-                throw new Exception("shipper not exist");
+                default:
+                    throw new Exception("shipper not exist");
             }
         }
  
